3e05110a4f8acb01fbd2a9b55ace220d168d3227
mifforiahubb <mifforiahubb@gmail.com>
2026-05-21 16:36:42 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "selling cheap robux ♡",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#2e56a8", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
